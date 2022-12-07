131732011847e6d8760863f973518d604391fe5e
homile <ssanho2@naver.com>
2022-12-07 23:47:18 +0900

    feat:Todo List 내용 수정 기능 구현

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -4,6 +4,11 @@ import React, { useEffect, useState } from "react";
 const Todo = () => {
   const [todoText, setTodoText] = useState("");
   const [todoList, setTodoList] = useState([]);
+  const [modify, setModify] = useState({
+    isModify: false,
+    id: null,
+    todo: "",
+  });
 
   // todo 생성
   const createTodo = (e) => {
@@ -29,6 +34,33 @@ const Todo = () => {
       .then((res) => setTodoList(res.data));
   };
 
+  // todo list 삭제
+  const deleteTodo = (id) => {
+    axios.delete(`https://pre-onboarding-selection-task.shop/todos/${id}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+  };
+
+  // 수정 버튼 클릭
+  const modifyHandler = (id) => {
+    setModify({ isModify: false, id });
+  };
+
+  // todo list 수정
+  const updateTodo = (id, isCompleted) => {
+    axios
+      .put(
+        `https://pre-onboarding-selection-task.shop/todos/${id}`,
+        { todo: modify.todo, isCompleted },
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }
+      )
+      .then((res) => {
+        setModify({ isModify: false });
+      });
+  };
+
   useEffect(() => {
     getTodos();
   }, []);
@@ -41,13 +73,34 @@ const Todo = () => {
       <div>
         <ul>
           {todoList.map((el) => {
-            console.log(el);
             return (
               <li key={el.id}>
                 <input type="checkbox"></input>
-                <label>{el.todo}</label>
-                <button>수정</button>
-                <button>삭제</button>
+                {modify.isModify || modify.id === el.id ? (
+                  <input
+                    defaultValue={el.todo}
+                    onChange={(e) =>
+                      setModify({ ...modify, todo: e.target.value })
+                    }
+                  ></input>
+                ) : (
+                  <label>{el.todo}</label>
+                )}
+                {modify.isModify || modify.id === el.id ? (
+                  <>
+                    <button onClick={() => setModify({ isModify: false })}>
+                      취소
+                    </button>
+                    <button onClick={() => updateTodo(el.id, el.isCompleted)}>
+                      저장
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button onClick={() => modifyHandler(el.id)}>수정</button>
+                    <button onClick={() => deleteTodo(el.id)}>삭제</button>
+                  </>
+                )}
               </li>
             );
           })}
