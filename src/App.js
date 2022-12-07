27c8ddc81f050662b3507ef1215101aa56c5f79d
homile <ssanho2@naver.com>
2022-12-07 22:22:53 +0900

    feat:todo 라우트 설정 및 파일,폴더 구조 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/auth/Auth";
+import Todo from "./pages/todo/Todo";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Auth />}></Route>
+        <Route path="/todo" element={<Todo />}></Route>
       </Routes>
     </div>
   );
