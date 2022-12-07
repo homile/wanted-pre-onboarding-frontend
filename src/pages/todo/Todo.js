a15c843bdcf64254db6d7718f9de8c4d3aa79cf8
homile <ssanho2@naver.com>
2022-12-07 23:00:36 +0900

    feat:Todo List 출력 기능 구현

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Todo = () => {
   const [todoText, setTodoText] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
   // todo 생성
   const createTodo = (e) => {
@@ -19,6 +20,19 @@ const Todo = () => {
     );
   };
 
+  // todo list get
+  const getTodos = () => {
+    axios
+      .get(`https://pre-onboarding-selection-task.shop/todos`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then((res) => setTodoList(res.data));
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <div>
       <div>
@@ -26,11 +40,17 @@ const Todo = () => {
       </div>
       <div>
         <ul>
-          <li>
-            <input type="checkbox"></input>
-            <button>수정</button>
-            <button>삭제</button>
-          </li>
+          {todoList.map((el) => {
+            console.log(el);
+            return (
+              <li key={el.id}>
+                <input type="checkbox"></input>
+                <label>{el.todo}</label>
+                <button>수정</button>
+                <button>삭제</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div>
