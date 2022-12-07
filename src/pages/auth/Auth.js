26f1d6eb0bbf66c05dacc291611b4c40966914ab
homile <ssanho2@naver.com>
2022-12-07 19:19:35 +0900

    feat:로그인/회원가입 기본 틀 구현

--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 /**
   '/' 경로에 로그인 / 회원가입 기능 개발
@@ -10,7 +10,40 @@ import React from "react";
  */
 
 const Auth = () => {
-  return <div>Auth</div>;
+  // 로그인/회원가입 탭 이동 상태
+  const [select, setSelect] = useState("SignIn");
+
+  return (
+    <div>
+      <div>
+        <span
+          onClick={() => {
+            setSelect("SignIn");
+          }}
+        >
+          로그인
+        </span>
+        <span
+          onClick={() => {
+            setSelect("SignUp");
+          }}
+        >
+          회원가입
+        </span>
+      </div>
+      <form>
+        <label htmlFor="email"></label>
+        <input id="email"></input>
+        <label htmlFor="password"></label>
+        <input id="password"></input>
+        {select === "SignIn" ? (
+          <button>로그인</button>
+        ) : (
+          <button>회원가입</button>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
